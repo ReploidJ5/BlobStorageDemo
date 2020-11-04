9cf2058a3f74b764fabc3254c0c101a7777144fb
Jose Lopez Cuevas <jose.lopez@conduent.com>
2020-11-04 17:03:43 -0600

    reorganized

---
+++ StorageApp/BlobManager.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageApp
+{
+    class BlobManager
+    {
+        IConfiguration configuration;
+        CloudStorageAccount storageAccount;
+        CloudBlobClient blobClient;
+        CloudBlobContainer container;
+
+        public BlobManager()
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("local.settings.json");
+
+            configuration = builder.Build();
+            storageAccount = CloudStorageAccount.Parse(configuration["StorageAccountConnectionString"]);
+            blobClient = storageAccount.CreateCloudBlobClient();
+            container = blobClient.GetContainerReference(configuration["ContainerName"]);
+        }
+      
+        public async Task<String> InitContainer()
+        {
+            bool created = await container.CreateIfNotExistsAsync();
+            return(created ? "Created the Blob container" : "Blob container already exists.");
+        }
+
+        public async Task<IEnumerable<string>> BlobList()
+        {
+            List<string> names = new List<string>();
+            BlobContinuationToken continuationToken = null;
+            BlobResultSegment resultSegment = null;
+            do
+            {
+                resultSegment = await container.ListBlobsSegmentedAsync(continuationToken);
+
+                // Get the name of each blob.
+                names.AddRange(resultSegment.Results.OfType<ICloudBlob>().Select(b => b.Name));
+
+                continuationToken = resultSegment.ContinuationToken;
+            } while (continuationToken != null);
+
+            return names;
+        }
+
+        public Task Save(Stream fileStream, string name)
+        {
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(name);
+            return blockBlob.UploadFromStreamAsync(fileStream);
+        }
+
+        public Task<Stream> Load(string name)
+        {
+            return container.GetBlobReference(name).OpenReadAsync();
+        }
+
+    }
+
+    
+}
---
+++ StorageApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StorageApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            BlobManager myManager = new BlobManager();
+
+            String result = await myManager.InitContainer();
+            Console.WriteLine(result);
+
+            IEnumerable<string> names = await myManager.BlobList();
+
+            foreach(var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+            string file = "./TextFile1.txt";
+            
+            FileStream streamer = new FileStream(file, FileMode.Open, FileAccess.Read);
+            await myManager.Save(streamer, "newfile");
+
+            Stream streamer2 = await myManager.Load("newfile");
+            using (var sr = new StreamReader(streamer2))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+
+        }
+    }
+}
